df501f008c144ff3de4ca2c0bec1a0a57e56b06e
WilliamMelo <WilliamMelo@hotmail.com>
2020-07-27 19:41:47 -0500

    test de crear una tarea

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe('crearNuevaTarea', () => {
+ 
+  it('Crear Tarea con nombre nuevo', () => {
+    expect("nuevo").toBeTruthy();
+  })
+   it('creando tarea con nombre vacio', () => {
+    expect("").toBeFalsy();
+  })
+
+})
+
